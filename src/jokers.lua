83fc4d075fecc22008ffec5f73e66c823a2e3017
cg223 <cg223celeste@gmail.com>
2025-10-30 23:28:24 -0400

    what is ts variable name

--- src/jokers.lua
+++ src/jokers.lua
@@ -1296,16 +1296,16 @@ SMODS.Joker {
         end
         if context.before and G.GAME.current_round.hands_played == 0 and #context.full_hand == 1 then
             G.playing_card = (G.playing_card and G.playing_card + 1) or 1
-            local copy_card = copy_card(context.full_hand[1], nil, nil, G.playing_card)
-            copy_card:add_to_deck()
+            local card_copied = copy_card(context.full_hand[1], nil, nil, G.playing_card)
+            card_copied:add_to_deck()
             G.deck.config.card_limit = G.deck.config.card_limit + 1
-            table.insert(G.playing_cards, copy_card)
-            G.hand:emplace(copy_card)
-            copy_card.states.visible = nil
+            table.insert(G.playing_cards, card_copied)
+            G.hand:emplace(card_copied)
+            card_copied.states.visible = nil
 
             G.E_MANAGER:add_event(Event({
                 func = function()
-                    copy_card:start_materialize()
+                    card_copied:start_materialize()
                     return true
                 end
             }))
@@ -1315,7 +1315,7 @@ SMODS.Joker {
                 func = function() -- This is for timing purposes, it runs after the message
                     G.E_MANAGER:add_event(Event({
                         func = function()
-                            SMODS.calculate_context({ playing_card_added = true, cards = { copy_card } })
+                            SMODS.calculate_context({ playing_card_added = true, cards = { card_copied } })
                             return true
                         end
                     }))
